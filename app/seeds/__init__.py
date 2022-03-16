f5ce784ea8346c2c9d0299237c8e4c6b5043bb99
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-03-16 11:14:11 -0400

    removed followers import into seed init.py

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,7 +3,6 @@ from .users import seed_users, undo_users
 from .posts import seed_posts, undo_posts
 from .comments import seed_comments, undo_comments
 from .likes import seed_likes, undo_likes
-from .follows import seed_follows, undo_follows
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
