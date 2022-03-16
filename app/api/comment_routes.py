a7cd36a3f73578fd3a20e047269c9eceaa4ab832
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-03-16 13:14:56 -0400

    finished backend routes for posts and comments

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -15,14 +15,13 @@ def get_all_post_comments(post_id):
     post_comments = Comment.query.filter(Comment.post_id==post_id).all()
     return {'post_comments': [comment.to_dict() for comment in post_comments]}
 
-
 @comment_routes.route('/<int:post_id>', methods=['POST'])
 @login_required
 def create_comment(post_id):
     """
     Creates a comment
     """
-    
+    user = User.query.get(current_user.id)
     form = CommentForm()
     
     form['csrf_token'].data = request.cookies['csrf_token']
@@ -36,11 +35,32 @@ def create_comment(post_id):
         
         db.session.add(comment)
         db.session.commit()
-        return comment.to_dict()
+        return {
+            "comment": comment.to_dict(), 
+            "user": user.to_dict()
+        }
     
 @comment_routes.route('<int:id>', methods=['DELETE'])
 def delete_comment(id):
+    """
+    Deletes a comment
+    """
     comment = Comment.query.get(id)
     db.session.delete(comment)
+    db.session.commit()
+    return comment.to_dict()
+
+
+@comment_routes.route('/<int:comment_id>', methods=['POST'])
+def update_comment(comment_id):
+    """
+    Updates a comment
+    """
+    form = CommentForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    comment = Comment.query.get(comment_id)
+    comment.comment_content = form.text_content
+
     db.session.commit()
     return comment.to_dict()
--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from xml.etree.ElementTree import Comment
+from app.forms.post_form import PostForm
 from flask import Blueprint, request
 from flask_login import login_required, current_user
 
@@ -7,11 +9,11 @@ from app.models import db, User, Post
 post_routes = Blueprint('posts', __name__)
 
 @post_routes.route('/')
-def get_all_posts():
+def get_followed_posts():
     """
-    Gets all posts
+    Gets all followed posts
     """
-    posts = Post.query.all()
+    posts = current_user.followed_posts()
     return {'posts': [post.to_dict() for post in posts]}
 
 @post_routes.route('/', methods=['POST'])
@@ -20,13 +22,51 @@ def create_post():
     """
     Creates a post
     """
-    post = Post(
-        user_id=current_user.id,
-        img_src=request.form['img_src'],
-        text_content=request.form['text_content'],
-        created_at=datetime.now()
-    )
+    form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     
-    db.session.add(post)
+    if form.validate_on_submit():
+        post = Post(
+            user_id=current_user.id,
+            img_src=request.form['img_src'],
+            text_content=request.form['text_content'],
+            created_at=datetime.now()
+        )
+        
+        user = User.query.get(current_user.id)
+
+        db.session.add(post)
+        db.session.commit()
+        return {
+            "post": post.to_dict(), 
+            "user": user.to_dict()
+        }
+        
+
+@post_routes.route('/<int:post_id>', methods=['DELETE'])
+@login_required
+def delete_post(post_id):
+    """
+    Deletes a post
+    """
+    post = Post.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    
+    return post.to_dict()
+
+@post_routes.route('/<int:post_id>', methods=['POST'])
+def update_post(post_id):
+    """
+    Updates a post
+    """
+    form = PostForm()
+    
+    form['csrf_token'].data = request.cookies['csrf_token']
+    post = Post.query.get(post_id)
+    post.img_src = form.img_src
+    post.text_content = form.text_content
+    
     db.session.commit()
     return post.to_dict()
+
